9a96caa542cd67c2a1976a9a1f9725636101d81a
Roberto <robertoarguera@gmail.com>
2022-06-01 19:35:10 -0600

    ...

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -125,22 +125,35 @@
             </svg>
           </div>
           <div class="col-md-6 text-center text-white">
-            <h3 class="fst-italic text-white">Contra Entrega</h3>
+            <h3 class="fst-italic text-white">Efectivo</h3>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="100"
               height="100"
               fill="currentColor"
-              class="bi bi-box2-heart"
+              class="bi bi-cash-coin"
               viewBox="0 0 16 16"
             >
               <path
-                d="M8 7.982C9.664 6.309 13.825 9.236 8 13 2.175 9.236 6.336 6.31 8 7.982Z"
+                fill-rule="evenodd"
+                d="M11 15a4 4 0 1 0 0-8 4 4 0 0 0 0 8zm5-4a5 5 0 1 1-10 0 5 5 0 0 1 10 0z"
               />
               <path
-                d="M3.75 0a1 1 0 0 0-.8.4L.1 4.2a.5.5 0 0 0-.1.3V15a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V4.5a.5.5 0 0 0-.1-.3L13.05.4a1 1 0 0 0-.8-.4h-8.5Zm0 1H7.5v3h-6l2.25-3ZM8.5 4V1h3.75l2.25 3h-6ZM15 5v10H1V5h14Z"
+                d="M9.438 11.944c.047.596.518 1.06 1.363 1.116v.44h.375v-.443c.875-.061 1.386-.529 1.386-1.207 0-.618-.39-.936-1.09-1.1l-.296-.07v-1.2c.376.043.614.248.671.532h.658c-.047-.575-.54-1.024-1.329-1.073V8.5h-.375v.45c-.747.073-1.255.522-1.255 1.158 0 .562.378.92 1.007 1.066l.248.061v1.272c-.384-.058-.639-.27-.696-.563h-.668zm1.36-1.354c-.369-.085-.569-.26-.569-.522 0-.294.216-.514.572-.578v1.1h-.003zm.432.746c.449.104.655.272.655.569 0 .339-.257.571-.709.614v-1.195l.054.012z"
+              />
+              <path
+                d="M1 0a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h4.083c.058-.344.145-.678.258-1H3a2 2 0 0 0-2-2V3a2 2 0 0 0 2-2h10a2 2 0 0 0 2 2v3.528c.38.34.717.728 1 1.154V1a1 1 0 0 0-1-1H1z"
+              />
+              <path
+                d="M9.998 5.083 10 5a2 2 0 1 0-3.132 1.65 5.982 5.982 0 0 1 3.13-1.567z"
               />
             </svg>
+          </div>
+          <div class="text-center mt-5">
+            <p class="text-white">
+              Para llevar a cabo tu pedido es necesario dar un porcentaje del
+              costo por adelantado.
+            </p>
           </div>
         </div>
       </div>
